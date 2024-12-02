5850f107f970c515c99c2a5a9a16644c6f5bce48
Josué Fernandes <josue7mf@hotmail.com>
2024-12-02 08:34:42 -0300

    fix: logs page doesn't show the error message

--- src/routes/logRoutes.js
+++ src/routes/logRoutes.js
@@ -5,11 +5,11 @@ import LogController from '../controllers/logController.js';
 
 const router = express.Router();
 const logController = new LogController();
-
+const ERROR_MESSAGE = "Você não tem permissão para acessar os logs da aplicação"
 router.get(
   '/',
   SessionMiddleware.ensureAuthenticated,
-  RoleMiddleware.ensureRole(['SUPERUSER', 'ADMIN']),
+  RoleMiddleware.ensureRole(['SUPERUSER', 'ADMIN'], ERROR_MESSAGE),
   (req, res) => logController.viewLogs(req, res)
 );
 
